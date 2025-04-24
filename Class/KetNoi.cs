cd276ca79ec5c2379a62760d86e37058e33fed67
User <User@DESKTOP-R3DMC9I>
2025-04-24 13:56:56 +0700

    Add project files.

---
+++ Class/KetNoi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace QLCHBanXeMay.Class
+{
+    internal class KetNoi
+    {
+        public static SqlConnection Conn;
+        public static string Connstring;
+        public static void Ketnoi()
+        {
+            Connstring = "Data Source=DESKTOP-R3DMC9I;Initial Catalog=QL_BanXeMay;Integrated Security=True ";
+            Conn = new SqlConnection();
+            Conn.ConnectionString = Connstring;
+            Conn.Open();
+            MessageBox.Show("Ket noi thanh cong");
+        }
+    }
+}
