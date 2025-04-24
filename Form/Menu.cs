53e908f6bfbe617fc1a678d754d1676b9303f634
TaHuyen04 <tahuyen1802@gmail.com>
2025-04-24 14:11:39 +0700

    set

--- Form/Menu.cs
+++ Form/Menu.cs
@@ -11,9 +11,9 @@ using QLCHBanXeMay.Class;
 
 namespace QLCHBanXeMay
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
